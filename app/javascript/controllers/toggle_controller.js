5dc8ef1b0b91baa5af1983e6d8d3618b7b71be50
Christian Bongard <christian.bongard90@gmail.com>
2024-03-05 09:54:31 +0400

    added the toggle element

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,6 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
+  static targets = ["toggleElement"]
+
   connect() {
+    console.log(this.toggleElement);
+  }
+
+  fire() {
+    this.toggleElement.classList.toggle("d-none");
   }
 }
