af625750ef5bcab425b6b57e07db375661ba8a6c
Christian Bongard <christian.bongard90@gmail.com>
2024-03-05 09:42:31 +0400

    added stimulus toggle controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :monuments
-  root to: "pages#home"
+  root to: "monuments#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
